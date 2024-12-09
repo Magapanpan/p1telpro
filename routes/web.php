9722df4a79570b01afae234599c90b1e30c1af4b
unknown <ah.fadhil2006@gmail.com>
2024-12-09 11:06:54 +0800

    database terpisah

--- routes/web.php
+++ routes/web.php
@@ -4,6 +4,10 @@ use App\Http\Controllers\AuthController;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\DataPegawaiController;
 
+Route::get('/location/kabupaten/{id_prov}', [DataPegawaiController::class, 'getKabupaten'])->name('location.kabupaten');
+Route::get('/location/kecamatan/{id_kab}', [DataPegawaiController::class, 'getKecamatan'])->name('location.kecamatan');
+Route::get('/location/kelurahan/{id_kec}', [DataPegawaiController::class, 'getKelurahan'])->name('location.kelurahan');
+
 Route::middleware('auth')->group(function () {
     Route::get('/data-pegawai', [DataPegawaiController::class, 'index'])->name('data-pegawai.index');
     Route::get('data-pegawai/{nik}/edit', [DataPegawaiController::class, 'edit'])->name('data-pegawai.edit');
@@ -13,8 +17,8 @@ Route::middleware('auth')->group(function () {
 
 Route::get('/register', [AuthController::class, 'showRegisterForm'])->name('register.form')->middleware(['auth', 'checkAdmin']);
 Route::post('/register', [AuthController::class, 'register'])->name('register')->middleware(['auth', 'checkAdmin']);
-Route::get('/login', [AuthController::class, 'showLoginForm'])->name('login.form')->middleware('guest');
-Route::post('/login', [AuthController::class, 'login'])->name('login');
+Route::get('/', [AuthController::class, 'showLoginForm'])->name('login.form')->middleware('guest');
+Route::post('/', [AuthController::class, 'login'])->name('login');
 Route::post('/logout', action: [AuthController::class, 'logout'])->name('logout')->middleware('auth');
 
 // Route protected untuk dashboard
